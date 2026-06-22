5dab7aadaec9364f8ed7048384ce3aa35402f7ed
Dev Armando <armandogranel2@gmail.com>
2026-06-23 00:00:19 +0800

    updating dark theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ import {
   Dot,
   BookOpen,
   Zap,
+  Users,
+  Coffee,
+  Infinity,
 } from "lucide-react";
 import { motion } from "motion/react";
 function App() {
@@ -132,9 +135,9 @@ function App() {
         initial={{ opacity: 0, translateY: 20 }}
         animate={{ opacity: 1, translateY: 0 }}
         transition={{ duration: 1, ease: "linear" }}
-        className="w-[45%] flex flex-col gap-5"
+        className="xs:w-[90%] sm:w-[85%] md:w-[80%] lg:w-[70%] xl:w-[65%] 2xl:w-[50%] flex flex-col gap-5"
       >
-        <div className="w-full flex gap-5 items-center border border-gray-300 dark:border-white/20 rounded-xl shadow p-5">
+        <div className="w-full flex gap-5  border border-gray-200 dark:border-white/20 rounded-xl shadow p-5">
           <div className="relative w-[20%] aspect-square">
             <img
               src={me}
@@ -150,26 +153,27 @@ function App() {
               }`}
             />
           </div>
-          <div className="flex flex-1 flex-col gap-2">
+          <div className="flex flex-1 flex-col gap-3">
             <div className="w-full flex flex-col gap-0.5">
               <span className="w-full flex items-center justify-between">
-                <h1 className="text-2xl font-bold">Armando Granel Jr</h1>
+                <h1 className="text-3xl font-bold" 
+                >Armando Granel Jr</h1>
                 <button
                   onClick={() => setDark(!dark)}
-                  className={`relative w-16 h-8 p-0.5 border transition-colors duration-400 outline-1 outline-transparent focus:outline-blue-500 ${
+                  className={`relative w-16 h-8 p-0.5 border transition-colors duration-400 ${
                     dark
                       ? "bg-gray-700 border-gray-600"
                       : "bg-gray-200 border-gray-300"
                   }`}
                 >
                   <div
-                    className="absolute top-1 left-1 w-6 h-6 bg-white flex text-muted items-center justify-center shadow-sm transition-all duration-400"
+                    className={`absolute top-1 left-1 w-6 h-6 bg-transparent flex ${dark ? "text-white" : "text-muted"} items-center justify-center shadow-sm transition-all duration-400`}
                     style={{
                       transform: dark ? "translateX(32px)" : "translateX(0px)",
                     }}
                   >
                     <span className="text-sm">
-                      {dark ? <Moon /> : <SunMedium />}
+                      {dark ? <SunMedium /> : <Moon />}
                     </span>
                   </div>
                 </button>
@@ -179,11 +183,11 @@ function App() {
               </span>
             </div>
             <span className="px-3 rounded-full py-1 dark:bg-white/10 dark:border-transparent bg-gray-100 self-start border-gray-300 border items-center gap-1 text-xs font-semibold flex ">
-              <Zap size={15} className="text-yellow-500"/> FULL STACK DEVELOPER
+              <Zap size={15} className="text-yellow-500" /> FULL STACK DEVELOPER
             </span>
-            <div className="w-full flex items-center gap-3">
+            <div className="w-full mt-auto flex items-center gap-3">
               <div className="flex items-center gap-2">
-                <span className="px-4 py-2 text-sm flex items-center gap-1 font-semibold bg-black text-white dark:bg-white dark:text-black ">
+                <span className="px-4 rounded-lg py-2 text-sm flex items-center gap-1 font-semibold bg-black text-white dark:bg-white dark:text-black ">
                   <Calendar size={15} /> Schedule a Call{" "}
                   <ArrowRight className="ml-2" size={15} />
                 </span>
@@ -202,38 +206,68 @@ function App() {
           </div>
         </div>
         <div className="flex gap-7 w-full">
-          <div className="w-[60%] flex flex-col px-5 py-6 shadow-sm gap-3  dark:bg-gray-200/10">
+          <div className="w-[60%] flex flex-col px-5 py-6 shadow-sm gap-3 border border-gray-200 dark:border-white/20  rounded-xl">
             <h1 className="text-lg font-bold">About</h1>
-            <p>
+            <p className="dark:text-white/60">
               I'm a passionate full-stack developer with expertise in building
               modern web applications using the MERN stack. I specialize in
               creating scalable, user-friendly solutions that solve real-world
               problems.
             </p>
-            <p>
+            <p className="dark:text-white/60">
               With a strong foundation in both front-end and back-end
               technologies, I focus on writing clean, maintainable code and
               delivering exceptional user experiences. I'm constantly learning
               and staying up-to-date with the latest industry trends and best
               practices.
             </p>
-            <p>
+            <p className="dark:text-white/60">
               As an upcoming third-year BSIT student, I’m still growing in my
               field while helping my classmates by sharing what I know and
               guiding them through topics they find difficult. Teaching others
               also helps me understand things better and supports a more
               collaborative learning environment.
             </p>
+            <div className="w-full grid grid-cols-4 gap-4">
+              <div className="flex items-center flex-col gap-1 py-3 bg-gray-200/70 dark:bg-white/15 dark:text- dark:border-white/30 border border-gray-300 rounded-xl">
+                <Code2 size={15} className="text-muted dark:text-white/45 " />
+                <h1 className="font-bold text-xl">7+</h1>
+                <p className="text-xs w-[70%] text-center text-muted dark:text-white/45 ">
+                  Projects Built
+                </p>
+              </div>
+              <div className="flex items-center flex-col gap-1 py-3 bg-gray-200/70 dark:bg-white/15 dark:text- dark:border-white/30 border border-gray-300 rounded-xl">
+                <Users size={15} className="text-muted dark:text-white/45 " />
+                <h1 className="font-bold text-xl">10+</h1>
+                <p className="text-xs  w-[70%] text-center text-muted dark:text-white/45 ">
+                  Classmates Mentored
+                </p>
+              </div>
+              <div className="flex items-center flex-col gap-1 py-3 bg-gray-200/70 dark:bg-white/15 dark:text- dark:border-white/30 border border-gray-300 rounded-xl">
+                <Coffee size={15} className="text-muted dark:text-white/45 " />
+                <Infinity />
+                <p className="text-xs  w-[70%] text-center text-muted dark:text-white/45 ">
+                  Cups of Coffee
+                </p>
+              </div>
+              <div className="flex items-center flex-col gap-1 py-3 bg-gray-200/70 dark:bg-white/15 dark:text- dark:border-white/30 border border-gray-300 rounded-xl">
+                <Code2 size={15} className="text-muted dark:text-white/45 " />
+                <h1 className="font-bold text-xl">7+</h1>
+                <p className="text-xs w-[70%] text-center text-muted dark:text-white/45 ">
+                  Projects Built
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="px-5 py-6 shadow-sm gap-5 flex flex-col flex-1 dark:bg-gray-200/10">
+          <div className="px-5 py-6 shadow-sm gap-5 flex flex-col flex-1  border border-gray-200 dark:border-white/20  rounded-xl">
             <div className="flex justify-between items-center ">
               <Expand />
               <span className="px-3 flex py-2  rounded-xl items-center text-xs font-semibold gap-2">
                 <Code2 size={16} /> Since 2025
               </span>
             </div>
-            <div className="w-full flex flex-col gap-2 pb-2 border-b border-slate-300">
-              <h1 className="text-xs font-semibold text-muted dark:text-gray-200/60">
+            <div className="w-full flex flex-col gap-2 pb-2 border-b border-slate-300 dark:border-gray-300">
+              <h1 className="text-xs font-semibold text-muted dark:text-white/45">
                 FULL STACK DEVELOPER
               </h1>
               <span>
@@ -242,27 +276,27 @@ function App() {
                   Granel Jr
                 </h1>
               </span>
-              <span className="flex items-center text-muted text-sm dark:dark:text-gray-200/60">
-                <Dot className="text-green-500" size={30} /> Open for
-                Opportunities
+              <span className="flex items-center text-muted text-sm gap-3 dark:text-white/45">
+                <span className="w-2 h-2 rounded-full bg-green-500 animate-pulse" />
+                <p> Open for Opportunities</p>
               </span>
             </div>
-            <div className="w-full flex flex-col gap-2 pb-2 border-b border-slate-300">
-              <span className="text-muted dark:text-gray-300 flex items-center gap-1 text-sm">
+            <div className="w-full flex flex-col gap-2 pb-2 border-b border-slate-300 dark:border-gray-300">
+              <span className="text-muted dark:text-white/45 flex items-center gap-1 text-sm">
                 <MapPin size={17} />
                 Rizal, Nueva Ecija, Philippines
               </span>
-              <span className="text-muted dark:text-gray-300 flex items-center gap-1 text-sm">
+              <span className="text-muted dark:text-white/45 flex items-center gap-1 text-sm">
                 <BookOpen size={17} /> 3rd Year BSIT Student
               </span>
-              <span className="text-muted dark:text-gray-300 flex items-center gap-1 text-sm">
+              <span className="text-muted dark:text-white/45 flex items-center gap-1 text-sm">
                 <Zap className="text-yellow-500" size={17} /> MERN STACK
                 DEVELOPER
               </span>
             </div>
             <div className="w-full flex flex-col gap-3">
               <div className="flex flex-col gap-1">
-                <h1 className="text-xs font-semibold text-muted dark:text-gray-200/60">
+                <h1 className="text-xs font-semibold text-muted dark:text-white/45">
                   AVAILABILITY
                 </h1>
                 <div className="flex items-center gap-2">
@@ -275,11 +309,52 @@ function App() {
                 </div>
               </div>
             </div>
+            <div className="flex flex-col gap-1">
+              <h2 className="text-xs dark:text-white/45 font-semibold  text-muted ">
+                Langauges
+              </h2>
+              <div className="flex flex-col">
+                <span className="flex items-center justify-between">
+                  <p className="text-sm dark:text-white/45 text-muted ">
+                    Filipino
+                  </p>
+                  <p className="text-sm dark:text-white/45 text-muted ">100%</p>
+                </span>
+                <span className="w-full h-1 rounded-full dark:bg-gray-300 bg-muted-foreground"></span>
+              </div>
+              <div className="flex flex-col">
+                <span className="flex items-center justify-between">
+                  <p className="text-sm  text-muted dark:text-white/45">
+                    English
+                  </p>
+                  <p className="text-sm  text-muted dark:text-white/45">50%</p>
+                </span>
+                <div className="w-full flex">
+                  <span className="w-1/2 h-1 rounded-l dark:bg-gray-300 bg-muted-foreground"></span>
+                  <span className="w-1/2 h-1 rounded-r dark:bg-white/20 bg-gray-100"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <h2 className="text-xs dark:text-white/45 font-semibold  text-muted ">
+                SOCIAL LINKS
+              </h2>
+              <div className="w-full grid grid-cols-2 gap-3">
+                {socials.map((social, _) => (
+                  <a
+                    href={social.link}
+                    className="shadow rounded-lg border border-gray-200 text-muted dark:text-white/45 dark:border-white/20 py-1 text-sm px-3 flex items-center gap-2"
+                  >
+                    <FontAwesomeIcon icon={social.icon} /> {social.name}
+                  </a>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-full flex gap-4">
+        <div className="w-full flex gap-4 ">
           <div className="flex  flex-col gap-2 w-[60%]">
-            <div className=" w-full flex flex-col px-5 py-6 shadow-sm gap-5 dark:bg-gray-200/10 ">
+            <div className=" w-full flex flex-col px-5 py-6 shadow-sm gap-5 border border-gray-200 dark:border-white/20  rounded-xl">
               <span className="w-full flex items-center justify-between">
                 <h1 className="text-md font-bold">Tech Stack</h1>
                 <span className="flex items-center gap-2 text-xs font-semibold">
@@ -288,30 +363,45 @@ function App() {
               </span>
               <div className="flex flex-col gap-3">
                 <div className="flex flex-col gap-2">
-                  <h1 className="text-sm font-bold">Frontend</h1>
+                  <h1 className="text-sm font-semibold text-muted dark:text-white/45">
+                    FRONTEND
+                  </h1>
                   <div className="flex gap-3 pl-2">
                     {frontend.map((item) => (
-                      <span key={item} className="py-1 px-2 shadow">
+                      <span
+                        key={item}
+                        className="text-xs font-semibold dark:text-gray-200 py-0.5 px-2 rounded-full  bg-slate-100 border border-slate-300 dark:bg-gray-200/10 dark:border-transparent "
+                      >
                         {item}
                       </span>
                     ))}
                   </div>
                 </div>
                 <div className="flex flex-col gap-2">
-                  <h1 className="text-sm font-bold">Backend</h1>
+                  <h1 className="text-sm font-semibold text-muted dark:text-white/45">
+                    BACKEND
+                  </h1>
                   <div className="flex gap-3 pl-2">
                     {backend.map((item) => (
-                      <span key={item} className="py-1 px-2 shadow">
+                      <span
+                        key={item}
+                        className="text-xs font-semibold dark:text-gray-200 py-0.5 px-2 rounded-full  bg-slate-100 border border-slate-300 dark:bg-gray-200/10 dark:border-transparent "
+                      >
                         {item}
                       </span>
                     ))}
                   </div>
                 </div>
                 <div className="flex flex-col gap-2">
-                  <h1 className="text-sm font-bold">Dev Tools</h1>
+                  <h1 className="text-sm font-semibold text-muted dark:text-white/45">
+                    DEV TOOLS
+                  </h1>
                   <div className="flex gap-3 pl-2">
                     {dev_tools.map((item) => (
-                      <span key={item} className="py-1 px-2 shadow">
+                      <span
+                        key={item}
+                        className="text-xs font-semibold dark:text-gray-200 py-0.5 px-2 rounded-full  bg-slate-100 border border-slate-300 dark:bg-gray-200/10 dark:border-transparent "
+                      >
                         {item}
                       </span>
                     ))}
@@ -319,7 +409,7 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className=" w-full flex flex-col px-5 py-6 shadow-sm gap-5 dark:bg-gray-200/10 ">
+            <div className=" w-full flex flex-col px-5 py-6 shadow-sm gap-5 border border-gray-200 dark:border-white/20  rounded-xl">
               <span className="w-full flex items-center justify-between">
                 <h1 className="text-md font-bold">Recent Project</h1>
                 <span className="flex items-center gap-2 text-xs font-semibold">
@@ -330,13 +420,13 @@ function App() {
                 {recent_projects.map((item, index) => (
                   <div key={index} className="shadow p-2 flex flex-col gap-2">
                     <h1 className="font-semibold text-sm">{item.title}</h1>
-                    <p className="text-sm text-muted dark:text-white">
+                    <p className="text-sm text-muted dark:text-white/45">
                       {item.desc}
                     </p>
                     <div className="flex flex-wrap gap-2">
                       {item.tools.map((tool, index) => (
                         <span
-                          className="py-1 px-3 shadow dark:bg-black"
+                          className="text-xs font-semibold dark:text-gray-200 py-0.5 px-2 rounded-full  bg-slate-100 border border-slate-300 dark:bg-gray-200/10 dark:border-transparent "
                           key={index}
                         >
                           {tool}
@@ -348,7 +438,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="h-fit flex-1 flex flex-col gap-3 p-3 shadow-sm hover:-translate-y-1  transition-all ease-in-out duration-200 dark:bg-gray-200/10">
+          <div className="h-fit flex-1 flex flex-col gap-3 p-3 shadow-sm hover:-translate-y-1  transition-all ease-in-out duration-200 border border-gray-200 dark:border-white/20  rounded-xl">
             <h1 className="text-md font-bold">Milestone</h1>
             <div className="flex flex-col ">
               {milestone.map((item, index) => (
@@ -374,26 +464,13 @@ function App() {
                       </h1>
                       <p className="text-sm">{item.year}</p>
                     </span>
-                    <p className="text-sm text-muted dark:text-white">
+                    <p className="text-sm text-muted dark:text-white/45">
                       {item.desc}
                     </p>
                   </div>
                 </div>
               ))}
             </div>
-          </div>
-        </div>
-        <div className="p-5 shadow dark:bg-gray-200/10 flex gap-3 ">
-          <div className="w-1/2 flex flex-col gap-2">
-            <h1 className="text-md font-bold">Social Links</h1>
-            {socials.map((social, _) => (
-              <a
-                href={social.link}
-                className="shadow py-2 px-3 flex items-center gap-2"
-              >
-                <FontAwesomeIcon icon={social.icon} /> {social.name}
-              </a>
-            ))}
           </div>
         </div>
       </motion.div>
